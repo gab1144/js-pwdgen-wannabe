4862bdf8dfb8764a59967b30497a188c3eb4d430
gab1144 <gabry.rinciari04@gmail.com>
2022-10-10 14:28:39 +0200

    [change] stile e output script

--- js/script.js
+++ js/script.js
@@ -5,9 +5,7 @@ const cognomeUtente = prompt("Qual'è il tuo cognome?");
 const colorePreferito = prompt("Qual'è il tuo colore preferito?");
 
 output = `
-  <strong> (insicurissimo) Password generator </strong> 
-  <br>
-  Password: ${nomeUtente}${cognomeUtente}${colorePreferito}22
+  ${nomeUtente}${cognomeUtente}${colorePreferito}22
 `;
 
 document.getElementById('password').innerHTML += output;
